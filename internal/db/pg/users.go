1a25b7e232732f1844394b1e5c9f7d3dcbf107d5
DJ Phoenix <phoenix@Dikes-MacBook-Air.local>
2024-12-06 07:01:24 +0300

    created user function

--- internal/db/pg/users.go
+++ internal/db/pg/users.go
@@ -1,1 +1,29 @@
 package pg
+
+import (
+	"context"
+	"fmt"
+
+	sq "github.com/Masterminds/squirrel"
+	gostreamv1 "github.com/lordvidex/gostream/pkg/api/gostream/v1"
+)
+
+// CreateUser ...
+func (r *Repository) CreateUser(ctx context.Context, p *gostreamv1.User) (uint64, error) {
+	q := sq.Insert("stream_users").
+		Columns("name", "age", "nationality").
+		Values(p.GetName(), p.GetAge(), p.GetNationality()).
+		Suffix("RETURNING id").
+		PlaceholderFormat(sq.Dollar)
+
+	query, params, err := q.ToSql()
+	if err != nil {
+		fmt.Println("sq error: ", err)
+		return 0, err
+	}
+	if err = r.pool.QueryRow(ctx, query, params...).Scan(&p.Id); err != nil {
+		return 0, err
+	}
+
+	return p.Id, nil
+}
