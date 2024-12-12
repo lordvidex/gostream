6d8578dd2f733b6fabee2a08dafda4ab453d4355
DJ Phoenix <phoenix@Dikes-MacBook-Air.local>
2024-12-13 00:57:14 +0300

    completed map.go

--- internal/db/inmemory/map.go
+++ internal/db/inmemory/map.go
@@ -16,36 +16,64 @@ type Map[K comparable, V Value[K]] struct {
 }
 
 func (m *Map[K, V]) Add(k K, v V) {
-	//TODO implement me
-	panic("implement me")
+	m.mu.Lock()
+	defer m.mu.Unlock()
+	m.data[k] = v
 }
 
 func (m *Map[K, V]) AddAll(vs []V) {
-	//TODO implement me
-	panic("implement me")
+	m.mu.Lock()
+	for _, v := range vs {
+		m.data[v.Key()] = v
+	}
+	m.mu.Unlock()
 }
 
 func (m *Map[K, V]) Remove(k K) {
-	//TODO implement me
-	panic("implement me")
+	m.mu.Lock()
+	defer m.mu.Unlock()
+	if m.data != nil {
+		delete(m.data, k)
+	}
 }
 
 func (m *Map[K, V]) Get(k K) (V, bool) {
-	//TODO implement me
-	panic("implement me")
+	m.mu.RLock()
+	defer m.mu.RUnlock()
+	for keys := range m.data {
+		if keys == k {
+			return m.data[keys], true
+		}
+	}
+	var v V // alternative to *new(V) but without alloc
+	return v, false
 }
 
 func (m *Map[K, V]) Clear() {
-	//TODO implement me
-	panic("implement me")
+	m.mu.Lock()
+	m.data = make(map[K]V)
+	m.mu.Unlock()
 }
 
 func (m *Map[K, V]) Snapshot() []V {
-	//TODO implement me
-	panic("implement me")
+	m.mu.RLock()
+	defer m.mu.RUnlock()
+	values := make([]V, 0, len(m.data))
+	for _, value := range m.data {
+		values = append(values, value)
+	}
+	return values
 }
 
 func (m *Map[K, V]) Iter() iter.Seq[V] {
-	//TODO implement me
-	panic("implement me")
+	return func(yield func(V) bool) {
+		m.mu.RLock()
+		defer m.mu.RUnlock()
+
+		for _, v := range m.data {
+			if !yield(v) {
+				return
+			}
+		}
+	}
 }
